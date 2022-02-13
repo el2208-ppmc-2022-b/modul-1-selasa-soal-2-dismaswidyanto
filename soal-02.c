91bb7033c0bcb7bea70733b279e3d8d841e591aa
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-02-13 23:10:54 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -8,27 +8,55 @@
 *Deskripsi        : -
 */
 
-// Library yang digunakan (jika diperlukan)
 #include <stdio.h>
 #include <math.h>
 
-// Main function
 int main() {
-	// deklarasi variabel
-	double ...;
-	...
+	double x1, y1;	// koordinat router
+	double x2, y2;	// koordinat komputer
+	double length;	// panjang kabel ethernet
 	
 	// Input koordinat router dan komputer
 	printf("Koordinat router (x,y): ");
-	scanf("...,...", ..., ...);
+	scanf("%lf,%lf", &x1, &y1);
 	printf("Koordinat komputer (x,y): ");
-	scanf("...,...", ..., ...);
+	scanf("%lf,%lf", &x2, &y2);
 	
-	// Algoritma perhitungan panjang kabel ethernet minimal
-	...
+	/* // Pengecekan input jika diperlukan (comment jika asumsi input pasti valid)
+	if (((x1 < 0 || x1 > 4 || y1 < 0 || y1 > 3) || ((x1 != 0 && x1 != 4) && (y1 != 0 && y1 != 3)))  ||
+	((x2 < 0 || x2 > 4 || y2 < 0 || y2 > 3) || ((x2 != 0 && x2 != 4) && (y2 != 0 && y2 != 3))))  {
+		printf("Input tidak valid");
+		return -1;
+	}
+	*/
+
+	// Kondisi router yang berseberangan merupakan kasus khusus dimana perhitungan
+	// panjang kabel tidak dapat dilakukan hanya dengan menggunakan selisih x dan y
+	// Kondisi router dan komputer bersebrangan (salah satu pada sumbu x)
+	if (((x1 == 0 && x2 == 4) || (x1 == 4 && x2 == 0)) && (y1 !=0 && y1 != 3 && y2 != 0 && y2 != 3)) {
+		if ((y1 + y2) < (3 - y1 + 3 - y2)) {
+			length = y1 + 4 + y2;
+		}
+		else {
+			length = 3 - y1 + 4 + 3 - y2;
+		}
+	}
+	// Kondisi router dan komputer bersebrangan (salah satu pada sumbu y)
+	else if (((y1 == 0 && y2 == 3) || (y1 == 3 && y2 == 0)) && (x1 != 0 && x1 != 4 && x2 != 0 && x2 != 4)) {
+		if ((x1 + x2) < (4 - x1 + 4 - x2)) {
+			length = x1 + 3 + x2;
+		}
+		else {
+			length = 4 - x1 + 3 + 4 - x2;
+		}
+	}
+	// Kondisi umum (dapat dihitung dengan menggunakan selisih x dan y)
+	else {
+		length = fabs(x1 - x2) + fabs(y1 - y2);
+	}
 	
-	// Template output
-	printf("Panjang kabel Ethernet minimal: ...", ...);
+	// Output panjang kabel ethernet minimal
+	printf("Panjang kabel Ethernet minimal: %lf", length);
 	
 	return 0;
 }
